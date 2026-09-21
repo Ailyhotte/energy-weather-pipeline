1796567cc895e798edffb3ff54576605f310ae94
legrobato_homeserver <elliot.callet@hotmail.com>
2026-09-21 08:12:01 +0000

    feat(deploy): deploy on server; docker and cron tasks for automation

--- src/config.py
+++ src/config.py
@@ -7,13 +7,6 @@ REGION_MAP = {
     "Bordeaux": "Nouvelle-Aquitaine",
 }
 
-
-DB_USER = os.getenv("POSTGRES_USER", "data_user")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "data_password")
-DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-DB_NAME = os.getenv("POSTGRES_DB", "energy_weather_db")
-
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
